914d0519cee617a6db13691d62bfc8ca8a05868b
vatsdivyansh <vatsdivyansh4@gmail.com>
2024-09-11 12:58:29 +0530

    Resource Icons added and prepared drawerItem for the drawer

---
+++ app/src/main/java/com/example/musicappui/Screen.kt
@@ -0,0 +1,16 @@
+package com.example.musicappui
+
+import androidx.annotation.DrawableRes
+
+sealed class Screen(val title: String ,  val route: String) {
+    sealed class DrawerScreen(val dTitle: String , val dRoute: String , @DrawableRes val icon:Int): Screen(dTitle,dRoute) {
+        object Account: DrawerScreen("Account" , "account" , R.drawable.ic_account)
+//        object Subscription: DrawerScreen("Subscription" , "subscribe" , R.drawable.ic_subscribe)
+        object Subscription: DrawerScreen("Subscription" , "subscribe" , R.drawable.ic_subscribe)
+//        object AddAccount: DrawerScreen("Add Account" , "add account" , R.drawable.ic_baseline_person_add_alt_1_24)
+object AddAccount: DrawerScreen("Add Account" , "add account" , R.drawable.baseline_person_add_alt_1_24)
+
+        // what are these ic's --> we need to create Icons --> go to  tools->resource manager -> use vector asset
+
+    }
+}
